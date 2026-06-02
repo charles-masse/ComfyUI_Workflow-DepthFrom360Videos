1891c78b9f32b23211c4e0221e8d8ff358550310
charles-masse <charlesmasseqc@gmail.com>
2026-06-02 11:24:15 -0400

    Loading module from github

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
 export default {
-  base: '/ComfyUI_Workflow-DepthFrom360Videos/',
-  resolve: {
-    alias: {
-      'three-6dof': new URL('../THREE.SixDOF/dist/three-6dof.es.js', import.meta.url).pathname
-    }
-  }
+  base: '/ComfyUI_Workflow-DepthFrom360Videos/'
 };
