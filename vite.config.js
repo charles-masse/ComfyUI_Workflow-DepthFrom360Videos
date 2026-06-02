17266557a763e81391379729e489545f7d2a25ba
charles-masse <charlesmasseqc@gmail.com>
2026-06-02 11:09:21 -0400

    Fixing alias for local module

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ export default {
   base: '/ComfyUI_Workflow-DepthFrom360Videos/',
   resolve: {
     alias: {
-      'three-6dof': new URL('../THREE.SixDOF/dist/index.js', import.meta.url).pathname
+      'three-6dof': new URL('../THREE.SixDOF/dist/three-6dof.es.js', import.meta.url).pathname
     }
   }
 };
